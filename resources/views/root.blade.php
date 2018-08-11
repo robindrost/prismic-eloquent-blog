5603fe5c49b034a903b556fa9b577cb421f26c5d
Robin Drost <robin.drost@oneshoe.nl>
2018-08-11 11:44:53 +0200

    Added fork me logo

--- resources/views/root.blade.php
+++ resources/views/root.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
@@ -10,12 +10,15 @@
     <link rel="stylesheet" type="text/css" href="{{ mix('css/site.css') }}">
 </head>
 <body class="bg-grey-lightest leading-normal">
+    @include('fork')
+
     @yield('content')
-    <footer class="text-center py-8 text-grey-dark text-xs">
+
+    <footer class="text-center py-8 text-grey-darker text-xs">
         Build with
-        <a class="text-grey-darker" href="https://github.com/robindrost/prismic-eloquent">Prismic Eloquent</a>,
-        <a class="text-grey-darker" href="https://laravel.com">Laravel</a> and
-        <a class="text-grey-darker" href="https://tailwindcss.com">Tailwind</a>
+        <a class="text-grey-darkest" href="https://github.com/robindrost/prismic-eloquent">Prismic Eloquent</a>,
+        <a class="text-grey-darkest" href="https://laravel.com">Laravel</a> and
+        <a class="text-grey-darkest" href="https://tailwindcss.com">Tailwind</a>
     </footer>
 
     <script src="{{ mix('js/app.js') }}"></script>
